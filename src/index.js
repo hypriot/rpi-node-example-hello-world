d260884911d7eb6c5f5fbaa15c658f8d53267c50
Stefan Scherer <scherer_stefan@icloud.com>
2015-03-08 12:18:05 +0100

    install morgan to have access log, if needed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var morgan = require('morgan');
 
 // Constants
 var PORT = 80;
 
 // App
 var app = express();
+
+// enable access log on STDOUT
+// app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
 - res.sendFile('/src/index.html');
 });
